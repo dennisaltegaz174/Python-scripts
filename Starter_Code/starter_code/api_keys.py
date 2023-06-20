46df4ec090bd4c6c9efdce4e00c6f55777b56992
Dennis Murithi <81504250+dennisaltegaz174@users.noreply.github.com>
2023-06-20 08:19:10 +0300

    Add files via upload
    asssing

---
+++ Starter_Code/starter_code/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "YOUR KEY HERE"
+
+# Geoapify API Key
+geoapify_key = "YOUR KEY HERE"
---
+++ steene2/Starter_Code/starter_code/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "7348715128cf50dbaf9a3fcc22f403e2"
+
+# Geoapify API Key
+geoapify_key = "5c1adc25c21646ea9f9fd334c3e41557"
